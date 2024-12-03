bb96d7e272d4aeddeb8a4ce4fad5f62335b8eb92
PriyankaAgwan30 <148351905+PriyankaAgwan30@users.noreply.github.com>
2024-12-03 13:53:28 +0530

    Update temparature_level.java
    Print statement edited

--- temparature_level.java
+++ temparature_level.java
@@ -2,15 +2,15 @@ import java.util.*;
 public class temparature_level {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter temparature in Farhanite : ");
+        System.out.println("Enter Temparature in Farhanite : ");
         int num = sc.nextInt();
 
         if(num < 98) {
-            System.out.println("Temparature is COLD !");
+            System.out.println("TEMPARATURE IS - COLD !");
         } else if(num >= 98 && num < 150) {
-            System.out.println("Temparature is WARM !");
+            System.out.println("TEMPARATURE IS - WARM !");
         } else {
-            System.out.println("Temparature is HOT !");
+            System.out.println("TEMPARATURE IS - HOT !");
         }
     }
 }
